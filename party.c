afddcfbb535fab0d023d3d4c1a7b21909e433f99
mohamedrazim11-maker <mohamedrazim11@gmail.com>
2025-10-25 16:17:02 +0530

    party.c included and some bugs fixed

---
+++ party.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>  
+#include<stdlib.h>
+
+
+void district(){
+       printf("\033[1;33m\n\n\t\t\tSelect The Distric From The List\033[0m\n\n ");
+       printf("\t\t\t|1.Matara  2.Kurunegala  3.Puttalam  4.Kegalle  5.Colombo|\n");
+      }
+void party(){
+       printf("\033[1;33m\n\n\t\t\tSelect The Party From The List\033[0m\n\n ");
+       printf("\t\t\t|1.A  2.B  3.C  4.D  5.E|\n\n");
+      }
+   
+
+
+
+
